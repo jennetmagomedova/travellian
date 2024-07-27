e69f29e32f34df8b3f82d128e60000f1cc65b611
Jennet Magomedova <jimagomedova@gmail.com>
2024-07-27 18:09:28 +0300

    added popular section

--- js/script.js
+++ js/script.js
@@ -12,3 +12,27 @@ $(function () {
     altFormat: "D, dth M yy",
   });
 });
+
+//swipper
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  slidesPerView: "auto",
+  loop: true,
+  spaceBetween: 32,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
